95c6e61b2e4244b18a3fd3bca93eb9cb15a08ff6
LloydBennett <lj_bennett@hotmail.co.uk>
2020-05-31 13:45:35 +0100

    added email validations

--- app/models/trainee.rb
+++ app/models/trainee.rb
@@ -1,4 +1,10 @@
 class Trainee < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presences: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
+
   has_many :training_sessions
   has_many :trainers, through: :training_sessions
 end
